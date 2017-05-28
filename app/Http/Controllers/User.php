e4c10e43aea06f2572d0bd2e170ec5ab22d3df32
José Neto <sputinykster@gmail.com>
2017-05-28 19:11:13 -0300

    Add user

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddUser;
+use App\Models\User as UserModel;
+
 class User extends Controller
 {
-    public function postAdd()
+    /**
+     * User Model
+     * @var Illuminate\Database\Eloquent\Model
+     */
+    private $userModel;
+    
+    public function __construct(UserModel $userModel) {
+        $this->userModel = $userModel;
+    }
+    
+    public function postAdd(AddUser $request)
     {
-        
+        $user = $this->userModel->create($request->only("username"));
+        return $user;
     }
 }
