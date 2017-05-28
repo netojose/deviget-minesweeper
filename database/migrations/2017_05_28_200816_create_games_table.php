ccf25b4205c5d3a0e9b6fc362f61f1453fd55b68
José Neto <sputinykster@gmail.com>
2017-05-28 17:14:28 -0300

    Making migrations and Models

---
+++ database/migrations/2017_05_28_200816_create_games_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('rows')->unsigned();
+            $table->integer('columns')->unsigned();
+            $table->integer('mines')->unsigned();
+            $table->json('elements');
+            $table->time('duration')->default('00:00:00');
+            $table->enum('finished', ['y', 'n'])->default('n');
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
