a4e3d6c0591f35410058b8d145a6549d343a0681
José Neto <sputinykster@gmail.com>
2017-05-28 23:13:54 -0300

    Adding SASS support

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -4,44 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Deviget - Minesweeper</title>
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: __dirname + '/resources/assets/js/index.jsx',
-  output: {
-    path: __dirname + '/public',
-    filename: 'app.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/
-      }
+    entry: __dirname + '/resources/assets/js/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: 'app.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({ filename: 'style.css', allChunks: true })
     ]
-  },
-  plugins: []
 };
